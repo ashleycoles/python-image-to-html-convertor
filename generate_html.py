7e33baeb2a6a652921a0726c35f779f91af12be8
Ashley Coles <ashley@acoles.co.uk>
2022-04-21 14:29:01 +0100

    Fixing rgba

--- generate_html.py
+++ generate_html.py
@@ -1,6 +1,13 @@
 def create_div_pixel(r, g, b, a=False) -> str:
-    if a:
-        return f"<i style='background: rgba({r}, {g}, {b}, {a});'></i>"
+    if a is not False:
+        if a == 255:  # If the pixel has no alpha, just use a hex code
+            hex_code = '#%02x%02x%02x' % (r, g, b)
+            return f"<i style='background: {hex_code};'></i>"
+        elif a == 0:  # If the pixel is totally invisible, just skip assigning a background
+            return f"<i></i>"
+        # Otherwise, use rgba
+        adjusted_a = a / 255  # the a in RGBA should be from 0 to 1, so account for that here
+        return f"<i style='background: rgba({r}, {g}, {b}, {adjusted_a});'></i>"
     else:
         hex_code = '#%02x%02x%02x' % (r, g, b)
         return f"<i style='background: {hex_code};'></i>"
