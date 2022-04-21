fe6858f17875cd9f0907865fb37f4fb48db72909
Ashley Coles <ashley@acoles.co.uk>
2022-04-21 11:54:18 +0100

    Switching to hex codes for non alhpa images to reduce HTML file size

--- generate_html.py
+++ generate_html.py
@@ -2,7 +2,8 @@ def create_div_pixel(r, g, b, a=False) -> str:
     if a:
         return f"<i style='background: rgba({r}, {g}, {b}, {a});'></i>"
     else:
-        return f"<i style='background: rgb({r}, {g}, {b});'></i>"
+        hex_code = '#%02x%02x%02x' % (r, g, b)
+        return f"<i style='background: {hex_code};'></i>"
 
 
 def generate_html_start(width: int, height: int) -> str:
