895cfcee70f70f67f4ccbc75b9ea1054f91c0f05
Ashley Coles <ashley@acoles.co.uk>
2022-04-21 11:39:00 +0100

    'optimizing' html

--- generate_html.py
+++ generate_html.py
@@ -1,12 +1,12 @@
 def create_div_pixel(r, g, b, a=False) -> str:
     if a:
-        return f"<div class='pixel' style='background-color: rgba({r}, {g}, {b}, {a});'></div>"
+        return f"<i style='background: rgba({r}, {g}, {b}, {a});'></i>"
     else:
-        return f"<div class='pixel' style='background-color: rgb({r}, {g}, {b});'></div>"
+        return f"<i style='background: rgb({r}, {g}, {b});'></i>"
 
 
 def generate_html_start(width: int, height: int) -> str:
-    return "<style>.pixel{ width: 1px; height: 1px; display: inline-block; } .img{ width: " + str(
+    return "<style>.img i{ width: 1px; height: 1px; display: inline-block; } .img{ width: " + str(
         width) + "px; height: " + str(height) + "px  }</style><body><div class='img'>"
 
 
